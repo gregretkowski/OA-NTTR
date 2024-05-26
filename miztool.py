f8b97bb33fd44850a6c8496763009c6efc42d667
Greg Retkowski <greg@rage.net>
2024-05-26 10:59:11 -0700

    starting work on miz tweaks
    * add github action to make releases
    * initial unpack of mizfile into miz directory
    * added python script (needed for github action)

---
+++ miztool.py
@@ -0,0 +1,165 @@
+import os
+import shutil
+from glob import glob
+import argparse
+
+
+#miz_subdir = 'OA-NTTR'
+#mizname = 'OA-NTTR'
+
+
+def find_dcs_directory():
+    home = os.environ['USERPROFILE']
+    saved_games = os.path.join(home, 'Saved Games')
+    dcs_openbeta = os.path.join(saved_games, 'DCS.openbeta')
+    dcs = os.path.join(saved_games, 'DCS')
+    candidate_dcs_dirs = [dcs_openbeta, dcs]
+    for candidate_dcs_path in candidate_dcs_dirs:
+        if os.path.exists(candidate_dcs_path):
+            return candidate_dcs_path
+    raise ValueError("Cannot find DCS saved games directory")
+
+
+def get_dcs_missions_dir():
+    dcs_dir = find_dcs_directory()
+    missions_dir = os.path.join(dcs_dir, 'Missions')
+    return missions_dir
+
+def load_variant_config(self):
+    with open('config/variants.yml') as f:
+        self.config = yaml.safe_load(f)
+        mission['mission'] = self.deep_merge(mission['mission'],val)
+
+def create_variants(self):
+    pass
+
+def deep_merge(dict1, dict2):
+    # Deep merge two dictionaries
+    result = dict1.copy()
+    for key, value in dict2.items():
+        if key in result and isinstance(result[key], dict) and isinstance(value, dict):            
+            result[key] = deep_merge(result[key], value)
+        else:
+            self.logger.debug("merge repleace key %s value %s with %s" % (key, result[key], value))
+            result[key] = value
+    return result
+    
+
+def canonical_path(p):
+    return os.path.normpath(os.path.abspath(p))
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Pack/unpack DCS mission from/to the git repo.')
+    
+    parser.add_argument('mizname', help='Name of the mission file/directory. ex: OA-NTTR, OA-Caucusus-Bactria')
+    command_group = parser.add_mutually_exclusive_group(required=True)
+    command_group.add_argument(
+        '--pack',
+        action='store_true',
+        help='Pack the miz file and copy to DCS saved games dir')
+
+    command_group.add_argument(
+        '--unpack',
+        action='store_true',
+        help='Extract the contents of the miz file from the DCS saved ' +
+        'games dir to the local repo.')
+    command_group.add_argument(
+        '--setversion',
+        action='store_true',
+        help='Updates dictionary to set a version string')
+    parser.add_argument(
+        '-v',
+        '--version',
+        nargs='?',
+        #const=os.getcwd(),
+        default=None,
+        help="Sets the version string")
+    parser.add_argument(
+        '-d',
+        '--directory',
+        nargs='?',
+        const=os.getcwd(),
+        help="If specified, copy the mission file into this directory.")
+
+    parser.add_argument(
+        '-f',
+        '--force',
+        action='store_true',
+        default=False,
+        help='Overwrite uncommitted changes when unpacking the mission')
+    args = parser.parse_args()
+
+    mizname = args.mizname
+    miz_subdir = args.mizname
+
+    miz_fullname = mizname + '.miz'
+    miz_fullpath = canonical_path(miz_fullname)
+
+    use_default_dir = args.directory is None
+    missions_dir = get_dcs_missions_dir(
+    ) if use_default_dir else args.directory
+    missions_dir = canonical_path(missions_dir)
+    if not os.path.exists(missions_dir):
+        os.makedirs(missions_dir)
+    if not os.path.isdir(missions_dir):
+        raise ValueError(f"{missions_dir} is not a directory")
+    miz_in_missions_dir = canonical_path(
+        os.path.join(missions_dir, miz_fullname))
+    miz_local = os.path.join(os.getcwd(), miz_fullname)
+
+    def pack():
+        shutil.make_archive(mizname, format='zip', root_dir=miz_subdir)
+        shutil.move(mizname + '.zip', miz_fullpath)
+        if os.path.exists(miz_in_missions_dir):
+            shutil.copyfile(dst=miz_in_missions_dir + '.backup',
+                            src=miz_in_missions_dir)
+        if miz_in_missions_dir != miz_fullpath:
+            shutil.copyfile(dst=miz_in_missions_dir, src=miz_fullpath)
+            os.remove(miz_local)
+
+    def unpack():
+        try:
+            import git
+        except ImportError:
+            pass
+        else:
+            repo = git.Repo(os.getcwd())
+            if not args.force and repo.is_dirty():
+                print(
+                    "Found untracked local changes, please commit or discard" +
+                    " before unpacking mission.")
+                exit(-1)
+
+        if miz_in_missions_dir != miz_fullname:
+            shutil.copyfile(src=miz_in_missions_dir, dst=miz_fullname)
+            shutil.unpack_archive(miz_fullname, miz_subdir, format='zip')
+            os.remove(miz_local)
+
+    def setversion():
+        if args.version is None:
+            print("No version specified, skipping")
+            return
+        else:
+            version = args.version
+            print(f"Setting version to {version}")
+            for filename in glob(f"{miz_subdir}/l10n/DEFAULT/dictionary", recursive=True):
+                with open(filename, 'r') as file:
+                    filedata = file.read()
+                filedata = filedata.replace('|PRERELEASE|', version)
+
+                with open(filename, 'w') as file:
+                    file.write(filedata)
+    if args.pack:
+        pack()
+    elif args.unpack:
+        unpack()
+    elif args.setversion:
+        setversion()
+    else:
+        assert (False)
+
+
+if __name__ == '__main__':
+    main()
